8ca895cd0bd0bfe1ada546da5a2274443b3e49d2
AndreMuderhwa <safariandre66@gmail.com>
2024-06-05 10:45:13 +0200

    add project

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ThemeProviderProps } from 'next-themes/dist/types';
+import { ThemeProvider as NextThemesProvider} from 'next-themes';
+
+
+export function ThemeProvider({children, ...props}:ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  )
+}
